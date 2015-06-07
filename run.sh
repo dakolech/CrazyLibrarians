5b388b0e277b879a1a82cf0b2981ba2c0689fde2
Bartosz Kostaniak <bartoszkostaniak@gmail.com>
2015-06-07 16:23:08 +0200

    Dwa parametry skryptu run.sh - liczba bibliotekarzy i liczba MPC

--- run.sh
+++ run.sh
@@ -1,12 +1,21 @@
 #!/bin/bash
 
 BIBLIOTEKARZE=100
+MPC=10
 
 if [ $# -eq 0 ]
 then
     echo "Nie podano liczby bibliotekrzy - ustawiono domyślną wartość $BIBLIOTEKARZE"
+    echo "Nie podano liczby MPC - ustawiono domyślną wartość $MPC"
 else
-    BIBLIOTEKARZE=$1
+    if [ $# -eq 1 ]
+    then
+        BIBLIOTEKARZE=$1
+        echo "Nie podano liczby MPC - ustawiono domyślną wartość $MPC"
+    else
+        BIBLIOTEKARZE=$1
+        MPC=$2
+    fi
 fi
 
-mpirun -np $BIBLIOTEKARZE debug/CrazyLibrarians -p 10
+mpirun -np $BIBLIOTEKARZE debug/CrazyLibrarians -p $MPC
