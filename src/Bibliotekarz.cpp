35305a161b7b27a152d643885e8e185e3af25be3
Bartosz Kostaniak <bartoszkostaniak@gmail.com>
2015-06-09 08:44:18 +0200

    Znowu się kompiluje

--- src/Bibliotekarz.cpp
+++ src/Bibliotekarz.cpp
@@ -1,6 +1,5 @@
 #include "Bibliotekarz.hpp"
 #include "Opcje.hpp"
-#define MSG_TAG 100
 
 #include <openmpi/mpi.h>
 #include <iostream>
@@ -23,7 +22,41 @@ void Bibliotekarz::odpowiedzInnymBibliotekarzom() {
 }
 
 void Bibliotekarz::poprosODostepDoMPC() {
-    wyswietlStan("Próbuję dostać się do sekcji krytycznej");
+    wyswietlStan("Proszę o dostęp do MPC");
+    
+    const int LICZBA_BIBLIOTEKARZY = Opcje::pobierzInstancje().pobierzLiczbeBibliotekarzy();
+    
+    liczbaCzytelnikowDoPonaglenia = rand()%100 + 1;
+    Wiadomosc zapytanie;
+    zapytanie.typ = RZADANIE;
+    zapytanie.tidNadawcy = tidRodzica;
+    zapytanie.zegarLamporta = wartoscZegaraLamporta;
+    zapytanie.aktualnaLiczbaWolnychMPC = liczbaDostepnychMPC;
+    zapytanie.liczbaCzytelnikowDoPonaglenia = liczbaCzytelnikowDoPonaglenia;
+
+    for(int i = 0; i < LICZBA_BIBLIOTEKARZY; i++) {
+        MPI_Send(&zapytanie, sizeof(zapytanie), MPI_BYTE, i, TypWiadomosci::RZADANIE, MPI_COMM_WORLD);
+    }
+
+    bool koniec = false;
+    int liczbaOdebranychWiadomosci = 0;
+    while(!koniec) {
+        int flag = 0;
+        Wiadomosc odpowiedz;
+        MPI_Status status;
+        MPI_Iprobe(MPI_ANY_SOURCE, MPI_ANY_TAG, MPI_COMM_WORLD, &flag, &status);
+        if (flag) {
+            MPI_Recv(&odpowiedz, sizeof(odpowiedz), MPI_BYTE, MPI_ANY_SOURCE, MPI_ANY_TAG, MPI_COMM_WORLD, &status);
+            liczbaOdebranychWiadomosci++;
+
+            ElementListy nowyElement;
+            nowyElement.idProcesu = odpowiedz.tidNadawcy;
+            nowyElement.liczbaCzytelnikowDoPonaglenia = odpowiedz.liczbaCzytelnikowDoPonaglenia;
+        }
+
+        if (liczbaOdebranychWiadomosci >= LICZBA_BIBLIOTEKARZY)
+          koniec = true;
+    }
 }
 
 void Bibliotekarz::uzywajMPC() {
@@ -76,47 +109,4 @@ void Bibliotekarz::wyswietlStan(string info) const {
     cout << "[Zegar: " << setw(4) << wartoscZegaraLamporta << "]"
          << "[tid: " << setw(4) << tidRodzica << "]"
          << info << "\n";
-}
-
-void Bibliotekarz::poprosODostepDoMPC() {
-    liczbaCzytelnikowDoPonaglenia = rand()%100 + 1;
-    Wiadomosc zapytanie;
-    zapytanie.typ = RZADANIE;
-    zapytanie.tidNadawcy = tidRodzica;
-    zapytanie.zegarLamporta = wartoscZegaraLamporta;
-    zapytanie.aktualnaLiczbaWolnychMPC = liczbaDostepnychMPC;
-    zapytanie.liczbaCzytelnikowDoPonaglenia = liczbaCzytelnikowDoPonaglenia;
-
-    for(int i=0; i<liczbaBibliotekarzy; i++) {
-        MPI_Send(&zapytanie, sizeof(zapytanie), MPI_BYTE, i, MSG_TAG, MPI_COMM_WORLD);
-    }
-
-    bool koniec = false;
-    int liczbaOdebranychWiadomosci = 0;
-    while(!koniec) {
-        int flag = 0;
-        Wiadomosc odpowiedz;
-        MPI_Status status;
-        MPI_Iprobe(MPI_ANY_SOURCE, MPI_ANY_TAG, MPI_COMM_WORLD, &flag, &status);
-        if (flag) {
-            MPI_Recv(&odpowiedz, sizeof(odpowiedz), MPI_BYTE, MPI_ANY_SOURCE, MPI_ANY_TAG, MPI_COMM_WORLD, &status);
-            liczbaOdebranychWiadomosci++;
-
-            ElementKolejki nowyElement;
-            nowyElement.idProcesu = odpowiedz.tidNadawcy;
-            nowyElement.liczbaCzytelnikowDoPonaglenia = odpowiedz.liczbaCzytelnikowDoPonaglenia;
-            nowyElement.pozwolilem = false;
-
-            for( list<ElementKolejki>::iterator iter=kolejka.begin(); iter != kolejka.end(); iter++ )
-                if ((iter->liczbaCzytelnikowDoPonaglenia > liczbaCzytelnikowDoPonaglenia &&
-                        iter->pozwolilem == false)
-                    ||
-                    (iter->liczbaCzytelnikowDoPonaglenia == liczbaCzytelnikowDoPonaglenia &&
-                        iter->idProcesu > tidRodzica && iter->pozwolilem == false))
-                  kolejka.insert(iter, nowyElement);
-        }
-
-        if (liczbaOdebranychWiadomosci >= liczbaBibliotekarzy)
-          koniec = true;
-    }
 }
